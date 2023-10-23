5672731d0d9ba44d86af096a49ec805c4f219d7d
italonogueira <147431079+italonogxz@users.noreply.github.com>
2023-10-23 13:56:12 -0300

    Add files via upload

---
+++ italo.py
@@ -0,0 +1,12 @@
+#Exercício Python 09: faça um software que verifique entre 2 numeros qual o maior
+
+numero1 = int(input("digite o primeiro numero inteiro"))
+numero2 = int(input("digite o segundo numero inteiro"))
+if numero1 > numero2:
+    print("o primeiro numero é maior que o segundo")
+else:
+    print("o primeiro numero é menor que o segundo:")
+
+
+
+
